acf0f5e569c4ee45a966c8f2c7bbf0ab5fe4692f
Dominic-Kishatingunimye <dominiqkish@gmail.com>
2022-06-30 17:55:55 -0700

    Another hot fix task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(void) {
+/**
+ * Using a 'puts' function and not a 'printf' function
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }

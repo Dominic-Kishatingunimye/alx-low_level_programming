236ca1aadc53453f2b92f5d9a0063ccd5d8043c0
Dominic-Kishatingunimye <dominiqkish@gmail.com>
2022-07-01 21:13:49 -0700

    Hot fix for task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
 /**
- * main -> a program that prints the alphabet in lowercase, followed by a new line.
- * You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
- * return: always 0
+ * main -> a program that prints the alphabet in lowercase,
+ * followed by a new line.
+ * can only use the putchar function 
+ * every other function (printf, puts, etc…) is forbidden
+ * Return: always 0
  */
 int main(void)
 {
 	int ch;
+
 	for (ch = 97; ch <= 122; ch++)	
 	{
 		putchar(ch);
 	}
 	putchar(10);
 
-	return(0);
+	return (0);
 }
 

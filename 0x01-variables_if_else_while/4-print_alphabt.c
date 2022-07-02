f63438d5773aaba10c64967033b423cf16ea0bec
Dominic-Kishatingunimye <dominiqkish@gmail.com>
2022-07-01 21:51:44 -0700

    task4 completed

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -> a program that prints the alphabet in lowercase,
+ * followed by a new line
+ * can only use the putchar function 
+ * every other function (printf, puts, etc…) is forbidden
+ * Return: always 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)	
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
+

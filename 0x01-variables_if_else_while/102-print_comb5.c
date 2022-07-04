2f73431fe831a97856f623c5e84fcbfd6435b4e4
Dominic-Kishatingunimye <dominiqkish@gmail.com>
2022-07-04 09:12:13 -0700

    Hot fixf for task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	int m, n;
 
-	for(m = 0; m <= 99; m++)
+	for (m = 0; m <= 99; m++)
 	{
 		for (n = 0; n <= 99; n++)
 		{
 			if (m < n && m != n)
 			{
 				putchar((m / 10) + '0');
-			        putchar((m % 10) + '0');
+				putchar((m % 10) + '0');
 				putchar(' ');
 				putchar((n / 10) + '0');
 				putchar((n % 10) + '0');

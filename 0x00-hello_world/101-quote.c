7925c5615363c96aad398fb3c2a13b131c42def7
Dominic-Kishatingunimye <dominiqkish@gmail.com>
2022-07-04 04:00:41 -0700

    Hot fix for task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,10 @@
  * You are not allowed to use any functions listed in
  * the NAME section of the man (3) printf or man (3) puts
  * Your program should return 1
+ * Return: always 0
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",  59);
 	return (1);
 }

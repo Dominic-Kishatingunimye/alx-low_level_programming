d7bbfbb4393067639a0f671be7ec11b73027b841
Dominic-Kishatingunimye <dominiqkish@gmail.com>
2022-07-05 07:58:25 -0700

    Creating the 'main.h', '_putchar.c' files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main -> Write a program that prints _putchar, 
@@ -7,6 +7,15 @@
  */
 int main (void)
 {
-	printf("_putchar\n");
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
+	putchar("\n");
 	return (0);
 }

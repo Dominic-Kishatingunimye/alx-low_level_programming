5186335acbabc5f2c6de14c3eaae7101422c5346
Dominic-Kishatingunimye <dominiqkish@gmail.com>
2022-07-05 06:59:29 -0700

    A C program to print '_putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -> Write a program that prints _putchar, 
+ * followed by a new line
+ * Return: always 0
+ */
+int main (void)
+{
+	printf("_putchar\n");
+	return (0);
+}

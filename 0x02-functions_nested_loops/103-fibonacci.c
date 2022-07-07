09b204daccf6b1eefa32bd10beea9b21f2598114
Dominic-Kishatingunimye <dominiqkish@gmail.com>
2022-07-06 21:54:34 -0700

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - find prints the sum of the even-valued terms
+ * followed by a new lin
+ * Return: always 0 on success
+ */
+int main(void)
+{
+	int i;
+	unsigned long int j, k, new, sum;
+
+	j = 1;
+	k = 2;
+	sum = 0;
+
+	for (i = 1, i <= 33; ++i)
+	{
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			sum = sum + j;
+		}
+		new = j + k;
+		j = k;
+		k = new;
+	}
+	printf("%lu\n", sum);
+
+	return (0);
+}

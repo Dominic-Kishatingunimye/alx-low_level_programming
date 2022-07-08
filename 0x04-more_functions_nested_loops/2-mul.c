35b0ad31d5941a5d5ce57093f0a9605d20a03aee
Dominic-Kishatingunimye <dominiqkish@gmail.com>
2022-07-08 02:04:30 -0700

    took Betty's advice

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
 /**
- * mul - function to multiply two int
+ * mul - function to multiply two integer
  * @a: integer  parameter1
  * @b: integer parameter2
  * Return: always success
  */
 int mul(int a, int b)
 {
-	return(a * b);
+	return (a * b);
 }

7da8952c70a8111f725d1b92abbca73ea914f411
Dominic-Kishatingunimye <dominiqkish@gmail.com>
2022-07-07 19:28:36 -0700

    Funtiion to check upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper -> fuction to check alphabet as upper or not
+ * @c: integer argument
+ * Return: returns anything on success
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}

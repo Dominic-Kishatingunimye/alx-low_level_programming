044f88351baf6b10f77ac778446083c451cfba7f
Dominic-Kishatingunimye <dominiqkish@gmail.com>
2022-07-12 08:08:45 -0700

    one last fix for task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - This function changes the integer to 98
- * @*n - a pointer to update integer value
+ * @n - parameter to be updated by pointer
  * Return: nothing
  */
 void reset_to_98(int *n)

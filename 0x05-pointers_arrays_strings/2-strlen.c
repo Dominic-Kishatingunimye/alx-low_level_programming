b40aa6da9b66f4e70641d27ba93efa8c151cfcdc
Dominic-Kishatingunimye <dominiqkish@gmail.com>
2022-07-12 20:15:39 -0700

    to count the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,14 +4,13 @@
 
 /**
  * _strlen -> a function that returns the length of a string
- * s: string length to be counted
+ * @s: string length to be counted
  * Return: always 0
  */
 int _strlen(char *s)
 {
 	int len;
 
-	len = strlen(*s);
-	printf(" %d\n", len);
+	for (len = 0; s[len] != '\0'; len++);
 	return (0);
 }

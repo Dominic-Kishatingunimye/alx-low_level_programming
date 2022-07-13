88d0dbe524eefb0ded089255299c438f33737de2
Dominic-Kishatingunimye <dominiqkish@gmail.com>
2022-07-12 20:24:47 -0700

    one last fix:task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
 /**
  * _strlen -> a function that returns the length of a string
  * @s: string length to be counted
- * Return: always 0
+ * Return: length of string
  */
 int _strlen(char *s)
 {
@@ -11,5 +11,5 @@ int _strlen(char *s)
 
 	for (len = 0; s[len] != '\0'; len++)
 		;
-	return (0);
+	return (len);
 }

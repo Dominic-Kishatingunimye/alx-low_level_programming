69b13c24ef200a0920a58ed41aab14c4b815696e
Dominic-Kishatingunimye <dominiqkish@gmail.com>
2022-07-15 08:02:43 -0700

    Hot fix for task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,2 +1,25 @@
 #include "main.h"
 
+/**
+ * _strncat - a function to catenates two strings
+ * @dest: destination
+ * @src: source of string
+ * @n: input of max bytes to be used
+ * Return: Always 0 success
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (j = 0; j < n; j++)
+	{
+		dest[i + j] = src[j];
+		if (src[j] == '\0')
+			j = n;
+	}
+	return (dest);
+}

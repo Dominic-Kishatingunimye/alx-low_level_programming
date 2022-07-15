920b324c3efdc72bff5d05244ccef6d2765d8586
Dominic-Kishatingunimye <dominiqkish@gmail.com>
2022-07-15 08:12:43 -0700

    Task2: function to copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncpy -> a fuction to copy a string
+ * @dest: destination string
+ * @src: source string
+ * @n: amount os bytes from source
+ * Return: the pointer to destination
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (destr);
+}
